2f2f142a4eb1764c946fdf32a0cf454be65b19be
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-01-20 22:41:24 -0600

    completed salary calculator

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 console.log("Hello world");
 let totalSalary = 0;
 function submit() {
-
-    console.log("clicked subnit")
+    console.log("clicked subnit");
     //get what the user typed in each of the input.
-    let firstNameElement = document.getElementById('firstNameInput');
-    let LastNameElement = document.getElementById('lastNameInput');
-    let IDElement = document.getElementById('idInput');
-    let TitleElement = document.getElementById('titleInput');
-    let AnnualSalaryElement = document.getElementById('annualSalaryInput');
+    let firstNameElement = document.getElementById("firstNameInput");
+    let LastNameElement = document.getElementById("lastNameInput");
+    let IDElement = document.getElementById("idInput");
+    let TitleElement = document.getElementById("titleInput");
+    let AnnualSalaryElement = document.getElementById("annualSalaryInput");
 
-    let tablebody = document.querySelector('tbody');
+    let tablebody = document.querySelector("tbody");
     tablebody.innerHTML += `
    <td>
    ${firstNameElement.value}
@@ -31,39 +30,31 @@ function submit() {
     <button onClick="deleteemployee(event)">delete</button>
    </td>
    `;
-    Calculatetotalsalary()
-    firstNameElement.value =  ""
-    LastNameElement.value = ""
-    IDElement.value = ""
-    TitleElement.value =""
-    AnnualSalaryElement.value =""
+    Calculatetotalsalary();
+    firstNameElement.value = "";
+    LastNameElement.value = "";
+    IDElement.value = "";
+    TitleElement.value = "";
+    AnnualSalaryElement.value = "";
 }
-
 
 function deleteemployee(event) {
     event.target.parentElement.parentElement.remove();
-    Calculatetotalsalary(true)
-};
+    Calculatetotalsalary(true);
+}
 function Calculatetotalsalary(isMinus = false) {
     totalSalary = 0;
-    const allSalaries = document.getElementsByClassName("salary")
+    const allSalaries = document.getElementsByClassName("salary");
     for (let index = 0; index < allSalaries.length; index++) {
         const element = allSalaries[index];
 
-
-        totalSalary = totalSalary + Number(element.textContent)
-
-
-
+        totalSalary = totalSalary + Number(element.textContent);
     }
-    const totalElement = document.getElementById("total")
-    totalElement.innerText = totalSalary
-    console.log(totalSalary);
-    if (totalSalary > 20000){ 
-        totalElement.classList.add("over-budget")
-    }
-    else {
-        totalElement.classList.remove("over-budget")
-
+    const totalElement = document.getElementById("total");
+    totalElement.innerHTML = `<h3>Total Monthly: ${totalSalary}</h3>`
+    if (totalSalary > 20000) {
+        totalElement.classList.add("over-budget");
+    } else {
+        totalElement.classList.remove("over-budget");
     }
 }
